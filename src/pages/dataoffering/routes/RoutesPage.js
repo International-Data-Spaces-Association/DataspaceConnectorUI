0e6cd8aa6cb5c883bc88fd04ab826d3d47053a5b
Bastian Weltjen <bastian.weltjen@fkie.fraunhofer.de>
2020-12-18 11:09:33 +0100

    changes in routes visualization

--- src/pages/dataoffering/routes/RoutesPage.js
+++ src/pages/dataoffering/routes/RoutesPage.js
@@ -26,7 +26,7 @@ export default {
             this.$refs.chart.add({
                 id: +new Date(),
                 x: 20,
-                y: 20,
+                y: 150,
                 name: 'Backend',
                 type: 'backendnode',
                 approvers: [],
@@ -36,43 +36,53 @@ export default {
             this.$refs.chart.add({
                 id: +new Date(),
                 x: 300,
-                y: 20,
+                y: 150,
                 name: 'App',
                 type: 'appnode',
                 approvers: [],
             });
         },
-        addResource() {
-            this.$refs.chart.add({
-                id: +new Date(),
-                x: 600,
-                y: 20,
-                name: 'Resource',
-                type: 'resourcenode',
-                approvers: [],
-            });
-        },
         render: function (g, node, isSelected) {
+            console.log(">>> ", g);
             node.width = node.width || 120;
-            node.height = node.height || 60;
+            node.height = node.height || 180;
             let borderColor = isSelected ? "#666666" : "#bbbbbb";
-            let body = g.append("rect").attr("class", "body");
-            body
-                .style("width", node.width + "px")
-            body
+            let resourceNode = g.append("rect").attr("class", "resourcenode");
+            resourceNode
+                .style("width", "100px");
+            resourceNode.style("height", "50px");
+            resourceNode
+                .attr("x", node.x + 10)
+                .attr("y", node.y);
+            resourceNode.attr("stroke", borderColor);
+
+            g.append("text")
+                .attr("x", node.x + node.width / 2)
+                .attr("y", node.y + 30)
+                .attr("text-anchor", "middle")
+                .attr("class", "unselectable")
+                .text(() => "Resource");
+
+            g.append("line").attr("class", "resourceline").attr("x1", node.x + 60)
+                .attr("y1", node.y + 60)
+                .attr("x2", node.x + 60)
+                .attr("y2", node.y + 50);
+
+            let nodeRect = g.append("rect").classed(node.type, true);
+            nodeRect
+                .style("width", "120px");
+            nodeRect.style("height", "120px");
+            nodeRect.attr("stroke", borderColor);
+            nodeRect
                 .attr("x", node.x)
-                .attr("y", node.y)
-                .classed(node.type, true)
-                .attr("rx", 30);
-            body.style("height", node.height + "px");
-            body.attr("stroke", borderColor);
+                .attr("y", node.y + 60);
             if (isSelected) {
-                body.classed("selectedNode", true);
+                nodeRect.classed("selectedNode", true);
             }
 
             g.append("text")
                 .attr("x", node.x + node.width / 2)
-                .attr("y", node.y + 20)
+                .attr("y", node.y + 80)
                 .attr("text-anchor", "middle")
                 .attr("class", "unselectable")
                 .text(() => node.name)
