8e3b91186fac325a76d3da9a4dbc1fe8bf810e33
Bastian Weltjen <bastian.weltjen@fkie.fraunhofer.de>
2021-03-11 11:35:29 +0100

    fix: do not use UI backend url if "#NOTSET#"

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -39,7 +39,7 @@ let backendUrl = "http://localhost:80";
 
 console.log("VUE_APP_UI_BACKEND_URL: ", process.env.VUE_APP_UI_BACKEND_URL);
 
-if (process.env.VUE_APP_UI_BACKEND_URL !== undefined) {
+if (process.env.VUE_APP_UI_BACKEND_URL !== undefined && process.env.VUE_APP_UI_BACKEND_URL != "#NOTSET#") {
     backendUrl = process.env.VUE_APP_UI_BACKEND_URL;
 }
 
