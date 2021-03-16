47430bb72de1682b8bd69dec5565369baae33fb2
Bastian Weltjen <bastian.weltjen@fkie.fraunhofer.de>
2021-03-16 10:18:10 +0100

    fix: settings validation problem

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -1,59 +1,63 @@
-    export default {
-        getRequiredRule() {
-            return [
-                v => !!v || 'This data is required'
-            ];
-        },
+export default {
+    getRequiredRule() {
+        return [
+            v => !!v || 'This data is required'
+        ];
+    },
 
-        getNumberRequiredRule() {
-            return [
-                v => !!v || 'This data is required',
-                v => /^[0-9]+$/.test(v) || 'Only numbers allowed',
-            ];
-        },
+    getNumberRequiredRule() {
+        return [
+            v => !!v || 'This data is required',
+            v => /^[0-9]+$/.test(v) || 'Only numbers allowed',
+        ];
+    },
 
-        getVersionRule() {
-            return [
-                v => /^[0-9.]+$/.test(v) || 'Only numbers and "." allowed',
-            ];
-        },
+    getVersionRule() {
+        return [
+            v => /^[0-9.]+$/.test(v) || 'Only numbers and "." allowed',
+        ];
+    },
 
-        getVersionRequiredRule() {
-            return [
-                v => !!v || 'This data is required',
-                v => /^[0-9.]+$/.test(v) || 'Only numbers and "." allowed',
-            ];
-        },
+    getVersionRequiredRule() {
+        return [
+            v => !!v || 'This data is required',
+            v => /^[0-9.]+$/.test(v) || 'Only numbers and "." allowed',
+        ];
+    },
 
-        getUrlListRule() {
-            return [
-                v => {
-                    let valid = true;
+    getUrlListRule() {
+        return [
+            v => {
+                let valid = true;
+                console.log(">>> V: ", v);
+                if (v.trim() != "") {
                     let split = v.split(',');
+                    console.log(">>> S: ", split);
                     for (let url of split) {
                         if (!(/^[h][t][t][p][s]{0,1}[:][/][/].*$/.test(url.trim()))) {
                             valid = /^[h][t][t][p][s]{0,1}[:][/][/].*$/.test(url.trim()) || 'Only URIs (http://... or https://...) allowed';
                             break;
                         }
                     }
-                    return valid;
                 }
-            ];
-        },
+                return valid;
+            }
+        ];
+    },
 
-        getUrlRequiredRule() {
-            return [
-                v => !!v || 'This data is required',
-                v => /^[h][t][t][p][s]{0,1}[:][/][/].*$/.test(v) || 'Only URIs (http://... or https://...) allowed',
-            ];
-        },
+    getUrlRequiredRule() {
+        return [
+            v => !!v || 'This data is required',
+            v => /^[h][t][t][p][s]{0,1}[:][/][/].*$/.test(v) || 'Only URIs (http://... or https://...) allowed',
+        ];
+    },
 
-        getUrlNotRequiredRule() {
-            return [
-                v => {
-                    if (v) return /^[h][t][t][p][s]{0,1}[:][/][/].*$/.test(v) || 'Only URIs (http://... or https://...) allowed'
-                    else return true;
-                }
-            ];
-        }
+    getUrlNotRequiredRule() {
+        return [
+            v => {
+                if (v) return /^[h][t][t][p][s]{0,1}[:][/][/].*$/.test(v) || 'Only URIs (http://... or https://...) allowed'
+                else return true;
+            }
+        ];
     }
+}
