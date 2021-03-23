1aee4cbbe2b551619480ff4dadec417d7c3ddd65
Bastian Weltjen <bastian.weltjen@fkie.fraunhofer.de>
2021-03-23 15:32:41 +0100

    chore: resource table: change column & filter from source type to file type

--- src/pages/dataoffering/resources/IDSResourcesPage.js
+++ src/pages/dataoffering/resources/IDSResourcesPage.js
@@ -21,7 +21,7 @@ export default {
             },
             {
                 text: 'Type',
-                value: 'sourceType'
+                value: 'fileType'
             }, {
                 text: 'Policy',
                 value: 'policyName'
@@ -36,7 +36,8 @@ export default {
             ],
             resources: [],
             filteredResources: [],
-            filterResourceType: null
+            filterResourceType: null,
+            fileTypes: ["All"]
         };
     },
     mounted: function () {
@@ -47,6 +48,10 @@ export default {
             this.$root.$emit('showBusyIndicator', true);
             dataUtils.getResources(resources => {
                 this.$data.resources = resources;
+                this.$data.fileTypes = ["All"];
+                for (let resource of resources) {
+                    this.$data.fileTypes.push(resource.fileType);
+                }
                 this.filterChanged();
                 this.$forceUpdate();
                 this.$root.$emit('showBusyIndicator', false);
@@ -58,7 +63,7 @@ export default {
             } else {
                 this.$data.filteredResources = [];
                 for (var resource of this.$data.resources) {
-                    if (resource.type == this.$data.filterResourceType) {
+                    if (resource.fileType == this.$data.filterResourceType) {
                         this.$data.filteredResources.push(resource);
                     }
                 }
