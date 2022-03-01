8dc795eb3270793b254d2cbf88ef990b67a2b784
Philipp Reusch <philipp.reusch@idsoc.de>
2022-03-01 16:55:31 +0100

    Fixed text in backend start log output.

--- src/backend/index.js
+++ src/backend/index.js
@@ -192,5 +192,5 @@ app.post('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Backend listening at http://localhost:${port}`)
 });
