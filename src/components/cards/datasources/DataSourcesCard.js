a0ef19b2913bed7611077c120b14753a77c866c9
Sebastian Opriel <sebastian.opriel@sovity.de>
2022-03-02 16:41:51 +0100

    feat: added functionality to display correct card value on dashboard

---
+++ src/components/cards/datasources/DataSourcesCard.js
@@ -0,0 +1,21 @@
+import DashboardCard from "@/components/cards/dashboardcard/DashboardCard.vue";
+import dataUtils from "@/utils/dataUtils";
+
+export default {
+    components: {
+        DashboardCard
+    },
+    data() {
+        return {
+            totalNumber: 0,
+        };
+    },
+    mounted: function () {
+        this.getData();
+    },
+    methods: {
+        async getData() {
+            this.$data.totalNumber = await dataUtils.getNumberOfDataSources();
+        },
+    }
+};
