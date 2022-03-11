547c504133f5fc34e0744b72275def76153216f6
Melissa Das <melissa.das@sovity.de>
2022-03-11 08:53:36 +0100

    Brokers transferred under Settings tab.

--- src/pages/PageStructure.js
+++ src/pages/PageStructure.js
@@ -1,17 +1,15 @@
 import DashboardPage from "@/pages/dashboard/DashboardPage.vue";
 import IDSResourcesPage from "@/pages/dataoffering/resources/IDSResourcesPage.vue";
 import AddResourcePage from "@/pages/dataoffering/resources/addresource/AddResourcePage.vue";
-import RoutesPage from "@/pages/dataoffering/routes/RoutesPage.vue";
-import AddRoutePage from "@/pages/dataoffering/routes/addroute/AddRoutePage.vue";
 import IDSDataConsumptionPage from "@/pages/dataconsumption/dataconsumption/IDSDataConsumptionPage.vue";
 import IDSResourcesPageConsumption from "@/pages/dataconsumption/resources/IDSResourcesPageConsumption.vue";
+import SettingsPage from "@/pages/settings/SettingsPage.vue";
+import BackendConnectionsPage from "@/pages/dataoffering/backendconnections/BackendConnectionsPage.vue";
+import ResourcePolicyPage from "@/pages/dataoffering/resources/addresource/policy/ResourcePolicyPage.vue";
 import BrokersPage from "@/pages/brokers/BrokersPage.vue";
 import AppsPage from "@/pages/apps/AppsPage.vue";
 import AppStoresPage from "@/pages/appstores/AppStoresPage.vue";
 import InstallAppsPage from "@/pages/appstores/installapps/InstallAppsPage.vue";
-import SettingsPage from "@/pages/settings/SettingsPage.vue";
-import BackendConnectionsPage from "@/pages/dataoffering/backendconnections/BackendConnectionsPage.vue";
-import ResourceCatalogsPage from "@/pages/dataoffering/resources/addresource/catalog/ResourceCatalogsPage.vue";
 
 export default {
     getPageStructure() {
@@ -29,11 +27,11 @@ export default {
             component: null,
             subpages: [{
                 path: "idsresourcesoffering",
-                name: "IDS Resources (Offering)",
+                name: "Offerings",
                 component: IDSResourcesPage,
                 subpages: [{
                     path: "addresource",
-                    name: "Add Resource",
+                    name: "New Offering",
                     component: AddResourcePage,
                     subpages: []
                 }, {
@@ -43,28 +41,9 @@ export default {
                     subpages: []
                 }]
             }, {
-                path: "backendconnectionsoffering",
-                name: "Backend Connections (Offering)",
-                component: BackendConnectionsPage
-            }, {
-                path: "routesoffering",
-                name: "Routes (Offering)",
-                component: RoutesPage,
-                subpages: [{
-                    path: "addrouteoffering",
-                    name: "Add Route (Offering)",
-                    component: AddRoutePage,
-                    subpages: []
-                }, {
-                    path: "showroute",
-                    name: "Show Route",
-                    component: AddRoutePage,
-                    subpages: []
-                }]
-            }, {
-                path: "catalogsoffering",
-                name: "Catalogs (Offering)",
-                component: ResourceCatalogsPage,
+                path: "policytemplates",
+                name: "Policy Templates",
+                component: ResourcePolicyPage,
                 subpages: []
             }]
         }, {
@@ -73,8 +52,8 @@ export default {
             icon: "mdi-file-document-outline",
             component: null,
             subpages: [{
-                path: "idsresourcesconsumption",
-                name: "IDS Resources (Consumption)",
+                path: "resourcerequests",
+                name: "Requests",
                 component: IDSResourcesPageConsumption,
                 subpages: [{
                     path: "requestresourceconsumption",
@@ -82,56 +61,63 @@ export default {
                     component: IDSDataConsumptionPage,
                     subpages: []
                 }]
-            }, {
-                path: "backendconnectionsconsumption",
-                name: "Backend Connections (Consumption)",
-                component: null
-            }, {
-                path: "routesconsumption",
-                name: "Routes (Consumption)",
-                component: RoutesPage,
-                subpages: [{
-                    path: "addrouteconsumption",
-                    name: "Add Route (Consumption)",
-                    component: AddRoutePage,
-                    subpages: []
-                }, {
-                    path: "showroute",
-                    name: "Show Route",
-                    component: AddRoutePage,
-                    subpages: []
-                }]
             }]
         }, {
-            path: "brokers",
-            name: "Brokers",
-            icon: "mdi-briefcase-variant-outline",
-            component: BrokersPage,
-            subpages: []
-        }, {
-            path: "apps",
-            name: "Apps",
-            icon: "mdi-apps",
-            component: AppsPage,
-            subpages: []
-        }, {
-            path: "appstores",
-            name: "App Stores",
-            icon: "mdi-storefront-outline",
-            component: AppStoresPage,
-            subpages: [{
-                path: "installapps",
-                name: "Install apps",
-                component: InstallAppsPage,
-                subpages: [],
-                showInMenu: false
-            }]
-        }, {
+
+                path: null,
+                name: "IDS Ecosystem",
+                icon: "mdi-briefcase-variant-outline",
+                component: null,
+                subpages: [
+                   {
+                        path: "apps",
+                        name: "Apps",
+                        icon: "mdi-apps",
+                        component: AppsPage,
+                        subpages: []
+                    }, {
+                        path: "appstores",
+                        name: "App Stores",
+                        icon: "mdi-storefront-outline",
+                        component: AppStoresPage,
+                        subpages: [{
+                            path: "installapps",
+                            name: "Install apps",
+                            component: InstallAppsPage,
+                            subpages: [],
+                            showInMenu: false
+                        }
+                        ]
+                    },
+
+                ]
+            },
+            {
             path: "settings",
             name: "Settings",
             icon: "mdi-cog-outline",
-            component: SettingsPage,
-            subpages: []
+            component: null,
+            subpages: [
+                {
+                    path: "general",
+                    name: "General",
+                    component: SettingsPage
+                }, {
+                    path: "backendconnectionsoffering",
+                    name: "Data Sources",
+                    component: BackendConnectionsPage
+                }, {
+                    path: "backendconnectionsconsumption",
+                    name: "Data Sinks",
+                    component: null
+                },
+                {
+                    path: "brokers",
+                    name: "Brokers",
+                    icon: "mdi-briefcase-variant-outline",
+                    component: BrokersPage,
+                    subpages: []
+                },]
         }
         ];
     },
